ef3867c7a1f4afa95454f044606ba74f1bc05a72
Andrew Thompson <andrew@tobarja.com>
2017-01-21 12:24:27 -0500

    module 3, end step 1

--- PTC.Data/TrainingProductViewModel.cs
+++ PTC.Data/TrainingProductViewModel.cs
@@ -7,9 +7,15 @@ namespace PTC.Data
         public string EventCommand { get; set; }
         public List<TrainingProduct> Products { get; set; }
         public TrainingProduct SearchEntity { get; set; }
+        public bool IsDetailAreaVisible { get; set; }
+        public bool IsListAreaVisible { get; set; }
+        public bool IsSearchAreaVisible { get; set; }
 
         public TrainingProductViewModel()
         {
+            IsListAreaVisible = true;
+            IsSearchAreaVisible = true;
+            IsDetailAreaVisible = false;
             Products = new List<TrainingProduct>();
             EventCommand = "list";
             ResetSearch();
@@ -27,6 +33,12 @@ namespace PTC.Data
                 case "resetsearch":
                     ResetSearch();
                     Get();
+                    break;
+
+                case "add":
+                    IsListAreaVisible = false;
+                    IsSearchAreaVisible = false;
+                    IsDetailAreaVisible = true;
                     break;
 
                 default:
